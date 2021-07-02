fe6a71037c4a7a5e73fcb5e5c835d286ef727463
daltonkell <daltonkell13@gmail.com>
2021-07-02 19:45:34 -0400

    Update Makefile, read sample file from CLI

--- Makefile
+++ Makefile
@@ -3,7 +3,10 @@ INCLUDE=-I./
 CFLAGS=-ggdb
 
 sha1.o: sha1.c
-	$(CC) -c $(CFLAGS) $(INCLUDES) $<
+	$(CC) -c $(CFLAGS) $(INCLUDE) $<
 
-TEST_SHA1: test_sha1.c sha1.o
-	$(CC) $(CFLAGS) -o $@ $^
+test_sha1.o: test_sha1.c
+	$(CC) -c $(CFLAGS) $(INCLUDE) $<
+
+TEST_SHA1: test_sha1.o sha1.o
+	$(CC) -o $@ $^
--- test_sha1.c
+++ test_sha1.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "sha1.h" /* SHA1_ */
 
-int main()
+int main(const int argc, const char *argv[])
 {
 
     /*
@@ -12,21 +12,62 @@ int main()
      */
     SHA1_SHA1Object_p_t sha1_p = (SHA1_SHA1Object_p_t)malloc(sizeof(SHA1_SHA1Object_t));
     SHA1_ERRCODE err = 0;
+    long int fsize = 0;       /* file size */
+    long int fsize_read = 0;  /* to check against fsize */
+    FILE *file_p = NULL; /* file pointer */
 
-    /* Initialize this message block for testing
-     * THIS WILL NOT APPEAR IN ALGORITHM!
+    /*
+     * Open file. If file is too large, say 5MB, do not allocate.
      */
-    char mblock[] = "this is my message and it's around 64 bytes long so let's copmute a HASH!";
-    printf("Original testing message:\n%s\n", mblock);
+    file_p = fopen(argv[1], "r");
+    if (file_p == NULL)
+    {
+        printf("Pointer to file is NULL! Exiting!\n");
+        return 1;
+    }
+    fseek(file_p, SEEK_SET, 0L);
+    fseek(file_p, 0L, SEEK_END);
+    fsize = ftell(file_p);
+
+    if (fsize > (5 * 1024 * 1024))
+    {
+        printf("File %s larger than 5MiB (%ld). Not allocating.\n", argv[1], fsize);
+        return 1;
+    }
+    printf("File size: %ld\n", fsize);
+
+    /*
+     * Return to beginning of file
+     * Allocate space for the file
+     */
+    fseek(file_p, SEEK_SET, 0L);
+    void *msg_p = malloc(fsize);
+    if (msg_p == NULL)
+    {
+        printf("Allocating buffer for message returned NULL!\n");
+        return 1;
+    }
+
+    /*
+     * Read data into msg_p array, close file
+     */
+    fsize_read = fread(msg_p, sizeof(char), fsize, file_p);
+    if (fsize_read != fsize)
+    {
+        printf("fread failed with %li\n", fsize_read);
+        return fsize_read;
+    }
+    fclose(file_p);
 
     /*
      * Invoke hash algorithm
      */
-    err = SHA1_process_message((const char *)mblock, sha1_p);
+    err = SHA1_process_message((const char *)msg_p, sha1_p);
 
     /*
      * free any dynamically allocated memory
      */
+    free(msg_p);
     free(sha1_p);
 
     return 0;
